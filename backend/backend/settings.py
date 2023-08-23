575f7edf18e28b3536b102516688d99e36474722
VeronikaLapteva <v.a.strelnikova@mail.ru>
2023-08-23 15:06:07 +0300

    updated settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,14 +1,17 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = os.getenv('SECRET_KEY', default=' ')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['51.250.103.160', '127.0.0.1', 'localhost', 'lbeebox.ddnsking.com']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1, localhost').split(',')
 
 
 # Application definition
